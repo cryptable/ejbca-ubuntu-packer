5fd974e58ac841cce3454ae2f145a65e1485a7b2
David (DDT) Tillemans <david.tillemans@cryptable.org>
2021-09-14 09:16:05 +0200

    Fixed type in the Environment variable of bash script

--- scripts/harden.sh
+++ scripts/harden.sh
@@ -4,12 +4,12 @@
 # TODO: create ejbca user with sudoers privileges
 
 export SALT=$(openssl rand -base64 16)
-sudo -E useradd -m -p `printf $EJBCA_USER_PASSWORD | openssl passwd -6 -salt $SALT -stdin` -s /bin/bash ejbca
+sudo -E useradd -m -p `printf ${EJBCA_USER_PASSWORD} | openssl passwd -6 -salt $SALT -stdin` -s /bin/bash ejbca
 sudo sed -i 's/^%sudo.*/%sudo ALL=(ALL:ALL) ALL/g' /etc/sudoers
 echo 'ejbca ALL=(ALL) ALL' | sudo tee /etc/sudoers.d/ejbca
 sudo chmod 440 /etc/sudoers.d/ejbca
 
-sudo -E useradd -M -p `printf $EJBCA_USER_PASSWORD | openssl passwd -6 -salt $SALT -stdin` -s /bin/bash ejbca-remote
+sudo -E useradd -M -p `printf ${EJBCA_USER_PASSWORD} | openssl passwd -6 -salt $SALT -stdin` -s /bin/bash ejbca-remote
 echo 'Defaults:ejbca-remote !requiretty' | sudo tee /etc/sudoers.d/ejbca-remote
 echo 'ejbca ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/ejbca-remote
 sudo chmod 440 /etc/sudoers.d/ejbca-remote
